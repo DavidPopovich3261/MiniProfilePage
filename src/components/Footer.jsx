1ae0fe59eb30ded4b4a1bd416141cc32e3e2e65d
DavidPopovich3261 <8534397@gmail.com>
2026-02-10 15:59:11 +0200

    123

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../css/Footer.css"
+function Footer() {
+  return (
+    <footer>“© 2025 My App</footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Heder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../css/Heder.css"
+function Heder() {
+    return (
+        <header>
+            <h1>My Profile Page</h1>
+        </header>
+    )
+}
+
+export default Heder
